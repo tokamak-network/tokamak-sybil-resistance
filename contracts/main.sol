0319beca62e981447c627a9bd29668f6a2f13fbe
jjamie65 <152574477+jjamie65@users.noreply.github.com>
2024-05-22 21:32:29 +0100

    Create main.sol

---
+++ contracts/main.sol
@@ -0,0 +1,25 @@
+// Last account index created inside the rollup
+    uint48 public lastIdx;
+
+    // Last batch forged
+    uint32 public lastForgedBatch;
+
+    // Each batch forged will have a correlated 'state root'
+    mapping(uint32 => uint256) public stateRootMap;
+
+    // Each batch forged will have a correlated 'exit tree' represented by the exit root
+    mapping(uint32 => uint256) public exitRootsMap;
+
+    // Each batch forged will have a correlated 'l1L2TxDataHash'
+    mapping(uint32 => bytes32) public l1L2TxsDataHashMap;
+
+    // Mapping of exit nullifiers, only allowing each withdrawal to be made once
+    // rootId => (Idx => true/false)
+    mapping(uint32 => mapping(uint48 => bool)) public exitNullifierMap;
+
+    // List of ERC20 tokens that can be used in rollup
+    // ID = 0 will be reserved for ether
+    address[] public tokenList;
+
+    // Mapping addres of the token, with the tokenID associated
+    mapping(address => uint256) public tokenMap;
