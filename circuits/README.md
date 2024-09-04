c7c6372e95f72aeea89ba0dc0d01d276eed54f95
jdhyun09 <jdhyun09@naver.com>
2024-09-04 13:45:40 +0900

    refactor: refactor related files

---
+++ circuits/README.md
@@ -0,0 +1,8 @@
+<p align="center">
+  <h1 align="center">
+    Tokamak-sybil-resistance Circuits
+  </h1>
+  <p align="center">All circuits related to Tokamak-sybil-resistance are managed in this folder.</p>
+</p>
+
+WIP: Will use [circomkit](https://github.com/erhant/circomkit) tools.
---
+++ circuits/src/index.ts
@@ -0,0 +1,15 @@
+import { Circomkit } from "circomkit";
+
+async function main() {
+    // create circomkit
+    const circomkit = new Circomkit({
+        protocol: "groth16",
+    });
+
+    // main()
+    // .then(() => process.exit(0))
+    // .catch((e) => {
+    //     console.error(e);
+    //     process.exit(1);
+    // });
+}
