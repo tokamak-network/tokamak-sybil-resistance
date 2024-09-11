2028f05a728049986a0e7e29ffb5229870b9fc01
shailu-s <emailid462@gmail.com>
2024-09-12 03:30:20 +0530

    Updated utils functionality in common package

--- go_sequencer/common/apitypes/apitypes.go
+++ go_sequencer/common/apitypes/apitypes.go
@@ -1,8 +1,21 @@
 package apitypes
 
-import "tokamak-sybil-resistance/common"
+import (
+	"math/big"
+	"tokamak-sybil-resistance/common"
+)
 
 type BigIntStr string
 
 // CollectedFeesAPI is send common.batch.CollectedFee through the API
 type CollectedFeesAPI map[common.TokenID]BigIntStr
+
+// NewBigIntStr creates a *BigIntStr from a *big.Int.
+// If the provided bigInt is nil the returned *BigIntStr will also be nil
+func NewBigIntStr(bigInt *big.Int) *BigIntStr {
+	if bigInt == nil {
+		return nil
+	}
+	bigIntStr := BigIntStr(bigInt.String())
+	return &bigIntStr
+}
---
+++ go_sequencer/common/utils.go
@@ -0,0 +1,22 @@
+package common
+
+<<<<<<< HEAD
+import (
+	"math/big"
+
+	ethCommon "github.com/ethereum/go-ethereum/common"
+)
+
+// EthAddrToBigInt returns a *big.Int from a given ethereum common.Address.
+func EthAddrToBigInt(a ethCommon.Address) *big.Int {
+	return new(big.Int).SetBytes(a.Bytes())
+=======
+// SwapEndianness swaps the order of the bytes in the slice.
+func SwapEndianness(b []byte) []byte {
+	o := make([]byte, len(b))
+	for i := range b {
+		o[len(b)-1-i] = b[i]
+	}
+	return o
+>>>>>>> 91ba93e (Updated utils functionality in common package)
+}
