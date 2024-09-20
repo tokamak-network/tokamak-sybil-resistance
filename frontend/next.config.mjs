a18cdcc42075f14127d04231405a556043a5cd6f
MightyDevKnight <james.mario.bello1209@gmail.com>
2024-09-20 18:37:27 +0200

    add vercel configuration

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    swcMinify: true,
+  },
+};
 
 export default nextConfig;
