63baf1d031479fc081b571e02ae6d00f13251c4d
Intizar <intizar.tashov22@gmail.com>
2024-10-31 20:41:08 +0900

    tmp bring back deleted tx types

--- go_sequencer/common/tx.go
+++ go_sequencer/common/tx.go
@@ -75,13 +75,25 @@ const (
 	// TxTypeExit represents L2->L1 token transfer.  A leaf for this account appears in the exit
 	// tree of the block
 	TxTypeExit TxType = "Exit"
+	// TxTypeTransfer represents L2->L2 token transfer
+	TxTypeTransfer TxType = "Transfer"
 	// TxTypeDeposit represents L1->L2 transfer
 	TxTypeDeposit TxType = "Deposit"
 	// TxTypeCreateAccountDeposit represents creation of a new leaf in the state tree
 	// (newAcconut) + L1->L2 transfer
 	TxTypeCreateAccountDeposit TxType = "CreateAccountDeposit"
+	// TxTypeCreateAccountDepositTransfer represents L1->L2 transfer + L2->L2 transfer
+	TxTypeCreateAccountDepositTransfer TxType = "CreateAccountDepositTransfer"
+	// TxTypeDepositTransfer TBD
+	TxTypeDepositTransfer TxType = "DepositTransfer"
+	// TxTypeForceTransfer TBD
+	TxTypeForceTransfer TxType = "ForceTransfer"
 	// TxTypeForceExit TBD
 	TxTypeForceExit TxType = "ForceExit"
+	// TxTypeTransferToEthAddr TBD
+	TxTypeTransferToEthAddr TxType = "TransferToEthAddr"
+	// TxTypeTransferToBJJ TBD
+	TxTypeTransferToBJJ TxType = "TransferToBJJ"
 	// TxTypeCreateVouch
 	TxTypeCreateVouch TxType = "CreateVouch"
 	// TxTypeDeleteVouch
