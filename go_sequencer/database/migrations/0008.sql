384cc454c095a06ed69d76bec7e8911221cde323
Intizar <intizar.tashov22@gmail.com>
2024-11-08 03:51:21 +0900

    rename go_sequencer to sequencer

--- go_sequencer/database/migrations/0008.sql
+++
@@ -1,9 +0,0 @@
--- +migrate Up
-ALTER TABLE tx_pool
-ADD COLUMN error_code NUMERIC,
-ADD COLUMN error_type VARCHAR;
-
--- +migrate Down
-ALTER TABLE tx_pool
-DROP COLUMN error_code,
-DROP COLUMN error_type;
