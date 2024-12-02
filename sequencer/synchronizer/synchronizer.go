c55539ff1d2f9540cb43d480d8636427760b5b11
kaiden <kaiden@tokamak.network>
2024-12-02 17:51:07 +1100

    feat/remove ProcessL1&L2Txs and update ProcessTxs to handle only L1UserTxs

--- sequencer/synchronizer/synchronizer.go
+++ sequencer/synchronizer/synchronizer.go
@@ -711,8 +711,7 @@ func (s *Synchronizer) rollupSync(ethBlock *common.Block) (*common.RollupData, e
 		tp := txprocessor.NewTxProcessor(s.stateDB, tpc)
 
 		// ProcessTxs updates poolL2Txs adding: Nonce (and also TokenID, but we don't use it).
-		processTxsOut, err := tp.ProcessTxs(
-			l1UserTxs, poolL2Txs)
+		processTxsOut, err := tp.ProcessTxs(l1UserTxs /*, poolL2Txs*/)
 		if err != nil {
 			return nil, common.Wrap(err)
 		}
