352f152f67472718ad01a10982a046c0630b684b
shailu-s <emailid462@gmail.com>
2024-12-06 12:36:25 +0530

    Updated history DB test

--- sequencer/common/pooll2tx.go
+++ sequencer/common/pooll2tx.go
@@ -124,7 +124,7 @@ func (tx *PoolL2Tx) SetID() error {
 // [ 32 bits ] signatureConstant // 4 bytes
 // Total bits compressed data:  225 bits // 29 bytes in *big.Int representation
 func (tx *PoolL2Tx) TxCompressedData(chainID uint64) (*big.Int, error) {
-	var b [29]byte
+	var b [35]byte
 
 	toBJJSign := byte(0)
 	pkSign, _ := babyjub.UnpackSignY(tx.ToBJJ)
@@ -150,8 +150,8 @@ func (tx *PoolL2Tx) TxCompressedData(chainID uint64) (*big.Int, error) {
 		return nil, Wrap(err)
 	}
 	copy(b[17:23], fromIdxBytes[:])
-	binary.BigEndian.PutUint64(b[23:25], chainID)
-	copy(b[25:29], SignatureConstantBytes[:])
+	binary.BigEndian.PutUint64(b[23:31], chainID)
+	copy(b[31:35], SignatureConstantBytes[:])
 
 	bi := new(big.Int).SetBytes(b[:])
 	return bi, nil
